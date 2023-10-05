e1437f22e8af0ac65af47f13ad98b08cdc701d00
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-10-05 18:14:57 -0300

    App in exclusive archive module

--- src/app.js
+++ src/app.js
@@ -4,4 +4,4 @@ const app = express();
 
 app.get('/', (request, response) => response.status(200).send('Hello World!'))
 
-app.listen(3000, () => console.log('Server is running on port 3000...'))
+module.exports = app;
