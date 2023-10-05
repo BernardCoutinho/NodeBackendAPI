036bf12ec8b832dfd8ce17340711c8bd92d030a6
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-10-05 18:54:21 -0300

    sintaxis correction

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-require('dotenv').config()
-const PORT = process.env.PORTA || 3000
+require('dotenv').config();
+const PORT = process.env.PORTA || 3000;
 
-app.listen(PORT, () => console.log('Server is running on port 3000...'))
+app.listen(PORT, () => console.log('Server is running on port 3000...'));
