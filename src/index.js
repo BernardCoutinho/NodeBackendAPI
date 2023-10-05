a6fd35bbc55061e68c15830482473fd35d732bdd
BernardCoutinho <bernard_coutinho2002@hotmail.com>
2023-10-05 18:03:33 -0300

    first get route

--- src/index.js
+++ src/index.js
@@ -2,4 +2,6 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (request, response) => response.status(200).send('Hello World!'))
+
 app.listen(3000, () => console.log('Server is running on port 3000...'))
